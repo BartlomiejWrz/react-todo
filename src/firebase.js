f311a4e10ea36b2831a79325e3cecae31169c4a5
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2018-01-26 01:36:08 +0100

    firebase updload

--- src/firebase.js
+++ src/firebase.js
@@ -15,12 +15,3 @@ export const database = firebase.database()
 export const auth = firebase.auth()
 
 export const storage = firebase.storage()
-/*
-apiKey: "AIzaSyApoBGQkLcEcTt30H2NrSHnGrwrOCwbD94",
-    authDomain: "first-personal-project.firebaseapp.com",
-    databaseURL: "https://first-personal-project.firebaseio.com",
-    projectId: "first-personal-project",
-    storageBucket: "first-personal-project.appspot.com",
-    messagingSenderId: "1096453058229"
-};
-    */
